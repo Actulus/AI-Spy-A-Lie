d89a09486f0c8b92df27f08ba8e0c323c473439d
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-02-13 21:27:39 +0200

    feat(backend): updated structure, and dependencies

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+import os
+from flask import jsonify
 
-app = Flask(__name__)
-CORS(app)
+# App Initialization
+from . import create_app # from __init__ file
+app = create_app(os.getenv("CONFIG_MODE"))
 
-@app.route('/hello', methods=['GET'])
-def get_hello():
+@app.route('/', methods=['GET'])
+def hello():
     return jsonify({'msg': 'Hello from Flask!'})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
