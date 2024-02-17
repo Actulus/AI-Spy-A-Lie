e9a02c8ebb099c0c228f6f197fac5efbdcb6f94d
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-02-17 16:36:21 +0200

    feat(frontend): added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import WelcomePage from "./components/Welcome";
+import HomePage from "./components/Home";
+import LoginPage from "./components/Login";
+import RegisterPage from "./components/Register";
+import NotFoundPage from "./components/NotFound";
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className="bg-gradient-to-b from-malachite to-dark-green min-h-screen min-w-screen">
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 
