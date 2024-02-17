6e33ec35822a321d384d1accf9c36a5f786531a4
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-02-17 16:48:51 +0200

    feat(frontend): added base colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'carmine' : '#921919',
+        'rose-taupe': '#8C5C5C',
+        'tea-rose': '#EDBBBB',
+        'nyanza': '#CEF2D6',
+        'spring-green': '#59EF7A',
+        'malachite': '#43DF65',
+        'dark-spring-green': '#396B38',
+        'pakistan-green': '#0F480E',
+        'dark-green': '#0A2610'
+      },
+    },
   },
   darkMode: "class",
   plugins: [],
