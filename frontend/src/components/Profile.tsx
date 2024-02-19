b1ee86f9a7d2ac192f8db07b3abff75fa5c05164
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-02-19 11:41:54 +0200

    feat(frontend): created base for profile page

---
+++ frontend/src/components/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ProfilePage: React.FC = () => {
+    return (
+        <div>
+        <h1 className="text-4xl text-gray-500">Profile</h1>
+        </div>
+    );
+};
+
+export default ProfilePage;
