677810aa6261fa57b0331a7d754d3a05c5bb452c
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-02-19 11:42:46 +0200

    feat(frontend): added new path to profile page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,24 @@ import HomePage from "./components/Home";
 import LoginPage from "./components/Login";
 import RegisterPage from "./components/Register";
 import NotFoundPage from "./components/NotFound";
+import Header from "./components/partials/Header";
+import Footer from "./components/partials/Footer";
+import ProfilePage from "./components/Profile";
 
 function App() {
   return (
     <Router>
-      <div className="bg-gradient-to-b from-malachite to-dark-green min-h-screen min-w-screen font-roboto">
+      <div className="bg-gradient-to-b from-malachite to-dark-green min-h-screen min-w-screen font-roboto flex flex-col justify-between">
+      <Header />
         <Routes>
           <Route path="/" element={<WelcomePage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
