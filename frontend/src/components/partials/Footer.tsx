6793d34740a902eb2351d86ef0e7f8a017f954e4
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-02-28 14:05:50 +0200

    fix(frontend): fixed layout

--- frontend/src/components/partials/Footer.tsx
+++ frontend/src/components/partials/Footer.tsx
@@ -2,11 +2,9 @@ import React from "react";
 
 const Footer: React.FC = () => {
     return (
-        <div>
-            <footer className="bg-spring-green text-dark-spring-green text-center font-concert-one flex justify-center items-center p-5 w-full h-20 mt-4">
-                <p>AI Spy A Lie &copy; 2024</p>
-            </footer>
-        </div>
+        <footer className="bg-spring-green text-dark-spring-green text-center font-concert-one flex justify-center items-center p-5 w-full h-20">
+            <p>AI Spy A Lie &copy; 2024</p>
+        </footer>
     )
 }
 
--- frontend/src/components/partials/Header.tsx
+++ frontend/src/components/partials/Header.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 
 const Header: React.FC = () => {
     const navigate = useNavigate();
+    const user = useKindeAuth().user;
+    const { logout } = useKindeAuth();
     const handleLogoClicked = () => {
         navigate('/home')
     }
@@ -12,12 +16,13 @@ const Header: React.FC = () => {
     }
 
     return (
-        <div>
-            <header className="bg-spring-green text-white text-center flex justify-between items-center p-5 w-full h-24 top-0">
-                <img src="/logo.png" alt="AI Spy A Lie Logo" onClick={handleLogoClicked} className='w-16 h-16 rounded-full drop-shadow-lg'/>
-                <img src="/cat_pfp.png" alt="Cat Profile Picture" onClick={handlePfpClicked} className='w-16 h-16 rounded-full drop-shadow-lg'/>
-            </header>
-        </div>
+        <header className="bg-spring-green text-white text-center flex justify-evenly md:justify-between items-center p-5 w-full h-24 top-0">
+            <img src="/logo.png" alt="AI Spy A Lie Logo" onClick={handleLogoClicked} className='w-16 h-16 rounded-full drop-shadow-lg' />
+            <div className="flex">
+                <Button name='logout-button' onClick={logout}>Logout</Button>
+                <img src={user?.picture || "/cat_pfp.png"} alt="Cat Profile Picture" onClick={handlePfpClicked} className='w-16 h-16 rounded-full drop-shadow-lg' />
+            </div>
+        </header>
     )
 }
 
