c3db3860a80a7924c1b7b7f5c1f0c0a7e228dd0f
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-05-18 22:31:12 +0300

    feat(backend): implemented endpoint to get the best high score for a player

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -13,9 +13,9 @@ router = APIRouter()
 class HighscoreCreate(BaseModel):
     user_name: str
     user_score: int
-    room_socket_id: UUID
+    room_socket_id: str
     ai_bot_type: str
-    kinde_uuid: UUID
+    kinde_uuid: str
     profile_picture: str
 
 class HighscoreResponse(BaseModel):
@@ -23,9 +23,9 @@ class HighscoreResponse(BaseModel):
     highscore_datetime: str
     user_name: str
     user_score: int
-    room_socket_id: UUID
+    room_socket_id: str
     ai_bot_type: str
-    kinde_uuid: UUID
+    kinde_uuid: str
     profile_picture: str
 
     @validator('highscore_datetime', pre=True, always=True)
@@ -43,11 +43,18 @@ def get_all_highscores(db: Session = Depends(get_db)):
     return highscores
 
 @router.get('/highscores/{kinde_uuid}', response_model=List[HighscoreResponse])
-def get_highscore_for_user(kinde_uuid: UUID, db: Session = Depends(get_db)):
+def get_highscore_for_user(kinde_uuid: str, db: Session = Depends(get_db)):
     highscores = db.query(Highscore).filter(Highscore.kinde_uuid == kinde_uuid).order_by(Highscore.user_score.desc()).order_by(Highscore.highscore_datetime.desc()).all()
     if not highscores:
         raise HTTPException(status_code=404, detail="No highscores found for this user")
     return highscores
+
+@router.get('/highscores/{kinde_uuid}/best', response_model=HighscoreResponse)
+def get_best_highscore_for_user(kinde_uuid: str, db: Session = Depends(get_db)):
+    highscore = db.query(Highscore).filter(Highscore.kinde_uuid == kinde_uuid).order_by(Highscore.user_score.desc()).first()
+    if not highscore:
+        raise HTTPException(status_code=404, detail="No highscores found for this user")
+    return highscore
 
 @router.post('/highscores', response_model=HighscoreResponse)
 def post_highscore(highscore: HighscoreCreate, db: Session = Depends(get_db)):
