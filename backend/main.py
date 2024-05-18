51b0084e18e714c54db5125d02ce05b297478816
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-05-18 20:35:42 +0300

    feat(backend): implemented conditional swagger

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,31 @@ import uvicorn
 import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
+from dotenv import load_dotenv
+import os
 from sockets import socketio_app
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
+# Conditionally include OpenAPI based on CONFIG_MODE
+config_mode = os.getenv('CONFIG_MODE')
+
+open_api_url = ""
+
+if config_mode == 'development':
+    open_api_url = '/openapi.json'
+
+# Initialize FastAPI app
+app = FastAPI(openapi_url=open_api_url)
+
+# Set custom OpenAPI schema
+
 app.mount('/sockets', app=socketio_app)
 
 app.add_middleware(
