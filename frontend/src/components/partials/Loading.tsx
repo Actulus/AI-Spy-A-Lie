c1dc133525c14be340c631db58e21e4cd2ec46dd
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-06-17 00:16:55 +0300

    fix(frontend): fixed loading screen

--- frontend/src/components/partials/Loading.tsx
+++ frontend/src/components/partials/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen">
+        <div className="flex items-center justify-center min-h-screen bg-spring-green">
             <div className="flex flex-col items-center">
                 <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-600">
                     <img src="pedro.png" alt="pedro" className="w-full h-full object-cover animate-[spin_60s_linear_infinite]" />
