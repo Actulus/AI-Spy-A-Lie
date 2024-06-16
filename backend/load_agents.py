19b8cd7dd72af5efed82e3813bbaf29581a266d8
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-06-16 14:49:56 +0300

    feat(backend): added logging for loading the agents

--- backend/load_agents.py
+++ backend/load_agents.py
@@ -5,6 +5,8 @@ from sarsa_agent import SARSAAgent
 from mcts_agent import MCTSAgent
 import numpy as np
 from collections import defaultdict
+import logging
+import os
 
 class CustomUnpickler(pickle.Unpickler):
     def find_class(self, module, name):
@@ -21,6 +23,13 @@ class CustomUnpickler(pickle.Unpickler):
         return super().find_class(module, name)
 
 def load_agents(easy_filename='q_learning_agent.pkl', medium_filename='dqn_agent.pkl', hard_filename='sarsa_agent.pkl'):
+    logging.debug(f"Checking if file exists: {easy_filename}, {medium_filename}, {hard_filename}")
+
+    # Check if file exists
+    if not os.path.exists(easy_filename) or not os.path.exists(medium_filename) or not os.path.exists(hard_filename):
+        logging.debug(f"File does not exist: {easy_filename}, {medium_filename}, {hard_filename}")
+        return None, None, None 
+    
     with open(easy_filename, 'rb') as f:
         q_table_dict = CustomUnpickler(f).load()
         easy_agent = QLearningAgent(state_size=7, action_size=132)  # Adjust state_size and action_size accordingly
