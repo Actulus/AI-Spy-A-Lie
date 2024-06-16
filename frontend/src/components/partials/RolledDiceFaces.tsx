3b1d2ae06a6412791f2afcbf9d3179df8bdba14b
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-06-16 23:28:44 +0300

    fix(frontend): fixed dice display

--- frontend/src/components/partials/RolledDiceFaces.tsx
+++ frontend/src/components/partials/RolledDiceFaces.tsx
@@ -4,7 +4,7 @@ const RolledDiceFaces = forwardRef<HTMLDivElement, { user: string; rolledDice: s
   ({ user, rolledDice}, ref) => {
     return (
       <div ref={ref} className={`flex flex-col items-center`}>
-        <p className="text-white text-2xl">{user} Dices</p>
+        <p className="text-white text-2xl">{user} Dice</p>
         <div className="flex flex-wrap justify-center items-center outline outline-white rounded-lg">
           {rolledDice.map((face, index) => (
             <div key={index} className="flex text-2xl m-2 p-2 text-center justify-center items-center bg-nyanza text-black rounded-lg">
