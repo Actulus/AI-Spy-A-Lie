1ee294627a9904337bfa052b475c79d5850fff7d
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-06-23 22:26:09 +0300

    feat(frontend): added preview to welcome page

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -7,7 +7,7 @@ const WelcomePage: React.FC = () => {
     const { login, register} = useKindeAuth();
 
     return (
-        <div className=''>
+        <div className='flex flex-col'>
             <div className='text-white flex flex-col items-center md:flex-row md:justify-between md:items-start'>
                 <img src="/logo.png" alt="AI Spy A Lie Logo" className='w-24 h-24 rounded-full md:float-left mt-5 drop-shadow-lg md:h-52 md:w-52 md:ml-8'/>
                 <div className='flex flex-col mt-5 md:mt-0 md:flex-row md:float-right md:mr-8 items-center'>
@@ -19,6 +19,9 @@ const WelcomePage: React.FC = () => {
                 <p className='text-3xl text-shadow shadow-black font-bold mb-2'>Unmask Deception, Sharpen Wit:
                 Where Minds Duel in the Art of Deceit!</p>
                 <p className='text-2xl text-balance'>Welcome to AI Spy A Lie, your digital arena for intense Liar's Dice battles against cunning AI opponents. Dive into a world where strategic thinking meets deception, challenging your wit with every roll of the dice. Whether you're a seasoned player or new to the game, AI Spy A Lie offers a dynamic platform to test your skills, sharpen your mind, and experience the thrill of outsmarting artificial intelligence. Join us, where victory depends on your ability to unveil the truth and master the art of bluffing!</p>
+            </div>
+            <div>
+                <img src='preview_2.png' alt='AI Spy A Lie Preview' className='w-1/2 h-auto m-5 float-right' />
             </div>
         </div>
     );
