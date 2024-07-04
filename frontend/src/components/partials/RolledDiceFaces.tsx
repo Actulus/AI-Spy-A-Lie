bcd7d19bae090d7355feca7a033884a498d5f826
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-07-04 14:12:34 +0300

    feat(frontend): added animation to dice rolls

--- frontend/src/components/partials/RolledDiceFaces.tsx
+++ frontend/src/components/partials/RolledDiceFaces.tsx
@@ -1,13 +1,42 @@
-import { forwardRef } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 
 const RolledDiceFaces = forwardRef<HTMLDivElement, { user: string; rolledDice: string[]; }>(
-  ({ user, rolledDice}, ref) => {
+  ({ user, rolledDice }, ref) => {
+    const [diceWithAnimation, setDiceWithAnimation] = useState(
+      rolledDice.map((face, index) => ({ face, key: index, animation: false }))
+    );
+
+    useEffect(() => {
+      // Apply the animation class when the rolledDice changes
+      setDiceWithAnimation(rolledDice.map((face, index) => ({
+        face,
+        key: index,
+        animation: true,
+      })));
+
+      // Remove the animation class after the animation duration
+      const timer = setTimeout(() => {
+        setDiceWithAnimation(rolledDice.map((face, index) => ({
+          face,
+          key: index,
+          animation: false,
+        })));
+      }, 1000); // match the animation duration
+
+      return () => clearTimeout(timer);
+    }, [rolledDice]);
+
     return (
-      <div ref={ref} className={`flex flex-col items-center`}>
+      <div ref={ref} className="flex flex-col items-center">
         <p className="text-white text-2xl">{user} Dice</p>
         <div className="flex flex-wrap justify-center items-center outline outline-white rounded-lg">
-          {rolledDice.map((face, index) => (
-            <div key={index} className="flex text-2xl m-2 p-2 text-center justify-center items-center bg-nyanza text-black rounded-lg">
+          {diceWithAnimation.map(({ face, key, animation }) => (
+            <div
+              key={key}
+              className={`flex text-2xl m-2 p-2 text-center justify-center items-center bg-nyanza text-black rounded-lg ${
+                animation ? 'animate-spin' : ''
+              }`}
+            >
               {face}
             </div>
           ))}
